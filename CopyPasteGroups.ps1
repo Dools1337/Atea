5617b88a89d9ef5c91eddc95ecdc82d01da21a7c
Timour <55139207+Dools1337@users.noreply.github.com>
2019-10-23 17:19:53 +0300

    Create CopyPasteGroups.ps1

---
+++ CopyPasteGroups.ps1
@@ -0,0 +1,6 @@
+import-Module ActiveDirectory  
+
+$copy = Read-host "Enter username to copy from: "  
+$paste  = Read-host "Enter username to copy to: "
+  
+Get-ADuser -identity $copy -properties memberof | select-object memberof -expandproperty memberof | Add-AdGroupMember -Members $paste
