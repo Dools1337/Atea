fc90f402ccff1cc1a8aa7cceff5ce3516e24d5e7
Timour <55139207+Dools1337@users.noreply.github.com>
2019-10-23 18:39:07 +0300

    Update CopyPasteGroups.ps1
    Formating and tightening this.

--- CopyPasteGroups.ps1
+++ CopyPasteGroups.ps1
@@ -1,6 +1,4 @@
 import-Module ActiveDirectory  
-
 $copy = Read-host "Enter username to copy from: "  
-$paste  = Read-host "Enter username to copy to: "
-  
+$paste  = Read-host "Enter username to copy to: "  
 Get-ADuser -identity $copy -properties memberof | select-object memberof -expandproperty memberof | Add-AdGroupMember -Members $paste
