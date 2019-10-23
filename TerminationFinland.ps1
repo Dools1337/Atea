dffd1c24da0bb4d22cf71ab77136302a682856db
Timour <55139207+Dools1337@users.noreply.github.com>
2019-10-23 17:05:08 +0300

    Create TerminationFinland.ps1
    Simple script to automate FI User Termination

---
+++ TerminationFinland.ps1
@@ -0,0 +1,20 @@
+cls
+Import-module activedirectory
+$j=Read-Host "Enter the samaccountname" 
+$k=Read-Host "Enter the account case for terminated user"
+$l=Read-Host "Enter the month for termination"
+$month = ($l)
+$ou = "OU=Users Disabled,OU=FI,OU=Country,DC=one,DC=local"
+$ou2 = 'OU=' + $month + ',' + $ou
+$i = Get-ADUser $j -Properties info | %{ $_.info}  
+Set-ADUser $j -Replace @{info="$($i) `r`n ($k) - Termination"} 
+$jgroups = Get-aduser $j -Properties MemberOf
+ForEach ($Group in $jgroups.memberof)
+{
+	Get-ADGroup $Group | Remove-ADGroupMember -confirm:$false -members $j
+	}
+Add-ADGroupMember -Identity DKHOS-SG-Stopped_Employees -members $j
+$ou_finland = "OU=Users Disabled,OU=FI,OU=Country,DC=one,DC=local"
+$path = (get-aduser $j).DistinguishedName
+Move-ADObject -Identity $path -TargetPath "$ou2" 
+Disable-adaccount $j
